4034fe0c7d65dfe5faab963842d6c0ccdacc3b93
Jinka <jinka97@gmail.com>
2025-06-14 22:25:01 +0100

    Initial commit since the migration...

---
+++ vite.config.js
@@ -0,0 +1,18 @@
+import { defineConfig } from 'vite'
+import tailwindcss from 'tailwindcss'
+import autoprefixer from 'autoprefixer'
+
+export default defineConfig({
+  css: {
+    postcss: {
+      plugins: [
+        tailwindcss,
+        autoprefixer
+      ]
+    }
+  },
+  build: {
+    outDir: 'dist',
+    emptyOutDir: true
+  }
+})
